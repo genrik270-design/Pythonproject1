aa38ddd0de29f0b92b5e68d622134803fa838e64
Tatyana Goncharova <genrik270@gmail.com>
2026-07-07 14:15:46 +0300

    Revert "Добавлена новая функция для обработки дат"

--- src/Function_date.py
+++
@@ -1,5 +0,0 @@
-def get_date(date_string: str) -> str:
-    """Преобразует дату через срезы строк."""
-    # date_string[:10] вернет "2026-06-29"
-    year, month, day = date_string[:10].split("-")
-    return f"{day}.{month}.{year}"
